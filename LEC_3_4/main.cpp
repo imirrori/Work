37d037ea1ff7e2c319fa700313c6b6b729659b84
Evgeniy Kozev <legionsnov@rambler.ru>
2022-02-05 14:05:50 +0300

    Проверка на полиндромность.

---
+++ LEC_3_4/main.cpp
@@ -0,0 +1,25 @@
+﻿// проверить на полиндром.
+
+#include <stdio.h>
+
+
+int main()
+{
+    long long int N;
+    long long int testN = 0;
+
+    printf("Please enter N: ");
+    scanf("%lld", &N);
+
+    long long int temp = N;
+    while(temp > 0)
+    {
+        testN *= 10;
+        testN += temp % 10;
+        temp /= 10;
+    }
+
+    printf("%s", N == testN ? "true" : "false");
+
+    return 0;
+}
