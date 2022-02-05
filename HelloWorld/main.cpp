b9d4b8a6daaad14bbe13fdcffdb925333aab0b20
Evgeniy Kozev <legionsnov@rambler.ru>
2022-02-05 12:48:44 +0300

    Поиск всех натуральных чисел

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,7 +1,33 @@
 ﻿#include "stdio.h"
 
+const int UP = 1;
+const int DOWN = 2;
+const int LEFT = 3;
+const int RIGHT = 4;
+
+int GetKey()
+{
+    return DOWN;
+}
+
 int main()
 {
-    printf("Hello World!");
+    switch(GetKey())
+    {
+    case DOWN:
+        printf("DOWN\n");
+        break;
+    case UP:
+        printf("UP\n");
+        break;
+    case LEFT:
+        printf("LEFT\n");
+        break;
+    case RIGHT:
+        printf("RIGHT\n");
+        break;
+    }
     return 0;
 }
+
+
---
+++ LEC_3_1/main.cpp
@@ -0,0 +1,18 @@
+﻿// Дано натуральное число N
+// Вывести все натуральные числа, на которые делится N, без остатка.
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned long long int N;
+    printf("Please enter N: ");
+    scanf("%llu", &N);
+    for (unsigned long long int i = 1; i < N; ++i)
+    {
+        if ((N % i) == 0)
+        {
+            printf("%llu\n", i);
+        }
+    }
+}
