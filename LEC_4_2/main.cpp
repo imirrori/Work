66e5df80fab20125b9689a929b9a10f89551a687
Evgeniy Kozev <legionsnov@rambler.ru>
2022-02-12 13:29:00 +0300

    Задача на двумерные массивы

---
+++ LEC_4_2/main.cpp
@@ -0,0 +1,31 @@
+﻿// Заполнить матрицу А(7х8) нулями и единицами по следующему правилу:
+// 0 - если сумма индексов элемента четная
+// 1 - если сумма индексов элемента не четная
+
+#include <stdio.h>
+
+int main()
+{
+    constexpr int X = 7;
+    constexpr int Y = 8;
+    int A[X][Y];
+
+    for(int i = 0; i < X; ++i)
+    {
+        for(int j = 0; j < Y; ++j)
+        {
+            A[i][j] = ((i + j) & 1) ? 1 : 0;
+        }
+    }
+
+    for(int i = 0; i < X; ++i)
+    {
+        for(int j = 0; j < Y; ++j)
+        {
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
